aba77a6a8e2c55d295e52ea0f95b93f5bd0cc7cb
SeJin12 <tpwls8122@naver.com>
2022-09-12 22:11:11 +0900

    variable

---
+++ src/var.ts
@@ -0,0 +1,8 @@
+// 변수 타입
+const str: string = "string";
+const num: number = 0;
+const numArr: number[] = [0, 1, 2];
+
+console.log(str, typeof str);
+console.log(num, typeof num);
+console.log(numArr, typeof numArr);
